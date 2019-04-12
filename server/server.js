1ad4a3bd362ad272726190dce6f89685d2ff926b
Druthi <polisettydruthi.k@gmail.com>
2019-04-11 19:29:24 -0700

    Wrote the searchZipcode API and returning data for zipcode

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const db = require('./db/db.js');
 const app = express();
 const cors = require('cors');
 const port = 3004;
@@ -8,6 +9,18 @@ const port = 3004;
 app.use(morgan());
 app.use(bodyParser());
 app.use(cors());
+
+
+app.get('/checkZipcode', (req, res) => {
+  let zipcode = req.query.zipcode;
+  db.query(`SELECT * FROM zipcodes where zipcode=${zipcode}`, (err, results) => {
+    if (err) {
+      throw err;
+    }
+    res.status(200).json(results);
+  });
+});
+
 
 app.use(`/products/:id`, express.static(__dirname + '/../public'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
