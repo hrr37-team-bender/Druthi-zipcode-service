090929fc3b2b2eb311dd25602da036fc5ccbd436
Druthi <polisettydruthi.k@gmail.com>
2019-04-11 17:03:58 -0700

    React setup complete

--- server/server.js
+++ server/server.js
@@ -9,4 +9,5 @@ app.use(morgan());
 app.use(bodyParser());
 app.use(cors());
 
+app.use(`/products/:id`, express.static(__dirname + '/../public'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
